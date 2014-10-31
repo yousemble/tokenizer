3e16f0279e56f76eec854afb4f08d37e6b85c093
christiaan-lombard <christiaan.lombard@yousemble.com>
2014-10-31 09:57:56 +0200

    added token contract

--- src/Yousemble/Tokenizer/Contracts/Token.php
+++ src/Yousemble/Tokenizer/Contracts/Token.php
@@ -1,24 +1,93 @@
+<?php namespace Yousemble\Tokenizer\Contracts;
+
+interface Token{
+
+
+  /*  Getters
+  -------------------------------------------------- */
+
+  /**
+   * Get the id attribute
+   * @return int
+   */
+  public function getId();
+
+  /**
+   * Get the hash attribute
+   * @return string
+   */
+  public function getHash();
+
+  /**
+   * Get the event-type attribute
+   * @return string
+   */
+  public function getEventType();
+
+  /**
+   * Get the expires_at timestamp attribute
+   * @return Carbon\Carbon
+   */
+  public function expiresAt();
+
+  /**
+   * Get the verified_at timestamp attribute
+   * @return Carbon\Carbon
+   */
+  public function verifiedAt();
+
+  /**
+   * Get the created_at timestamp attribute
+   * @return Carbon\Carbon
+   */
+  public function createdAt();
+
+  /**
+   * Check if the token is expired,
+   * an expired token may not be verified
+   *
+   * @return boolean
+   */
+  public function isExpired();
+
+  /**
+   * Check if the token is verified,
+   * a verified token may not be expired
+   *
+   * @return boolean
+   */
+  public function isVerified();
 
 
 
-public function verify();
+  /*  Model actions
+  -------------------------------------------------- */
 
-public function expire();
+  /**
+   * Verify the token, setting the verified_at attribute to now()
+   * @return Token This token
+   * @throws TokenIsExpiredException If the token is expired
+   */
+  public function verify();
 
-public function delete();
+  /**
+   * Expire the token, setting the expired_at attribute to now()
+   *
+   * @return Token This token
+   * @throws TokenIsVerifiedException If the token is already verified
+   */
+  public function expire();
 
-public function getId();
 
-public function getHash();
+  /**
+   * Delete the token
+   * @return void
+   */
+  public function delete();
 
-public function getEventType();
+}
 
-public function expiresAt();
 
-public function verifiedAt();
 
-public function createdAt();
 
-public function isExpired();
 
-public function isVerified();
